4b8dc787a22b2d013dabe551ef3e6157ebfab8d9
Sanket Zaware <sanketzaware12@gmail.com>
2026-08-06 18:00:00 +0530

    Added PalindromeString

--- src/com/logical/MissingNumberXOR.java
+++ src/com/logical/MissingNumberXOR.java
@@ -1,5 +1,39 @@
 package com.logical;
 
+import java.util.Scanner;
+
 public class MissingNumberXOR {
+	
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int[] numbers = new int[n - 1];
+
+        System.out.println("Enter " + (n - 1) + " numbers:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int xor = 0;
+
+        // XOR of numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            xor = xor ^ i;
+        }
+
+        // XOR with array elements
+        for (int number : numbers) {
+            xor = xor ^ number;
+        }
+
+        System.out.println("Missing Number: " + xor);
+
+        sc.close();
+    }
 
 }
---
+++ src/com/logical/PalindromeString.java
@@ -0,0 +1,29 @@
+package com.logical;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reverse = reverse + text.charAt(i);
+        }
+
+        if (text.equals(reverse)) {
+            System.out.println(text + " is a Palindrome String");
+        } else {
+            System.out.println(text + " is Not a Palindrome String");
+        }
+
+        sc.close();
+    }
+	
+}
