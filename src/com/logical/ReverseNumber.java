82782231fe01fec93dfd6b977228be68750c91e9
Sanket Zaware <sanketzaware12@gmail.com>
2026-09-03 15:55:39 +0530

    Add reverse number program

---
+++ src/com/logical/ReverseNumber.java
@@ -0,0 +1,29 @@
+package com.logical;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	
+	 public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int number = sc.nextInt();
+
+	        int originalNumber = number;
+	        int reverse = 0;
+
+	        while (number != 0) {
+	            int digit = number % 10;
+	            reverse = reverse * 10 + digit;
+	            number = number / 10;
+	        }
+
+	        System.out.println("Original Number: " + originalNumber);
+	        System.out.println("Reversed Number: " + reverse);
+
+	        sc.close();
+	    }
+
+}
