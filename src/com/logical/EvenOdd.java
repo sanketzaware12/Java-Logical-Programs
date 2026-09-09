1ffd5035c43ae5a43b22ba2db78b1a449aabc465
Sanket Zaware <sanketzaware12@gmail.com>
2026-09-09 12:36:11 +0530

    Added evenodd program

---
+++ src/com/logical/EvenOdd.java
@@ -0,0 +1,23 @@
+package com.logical;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+	
+	 public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int number = sc.nextInt();
+
+	        if (number % 2 == 0) {
+	            System.out.println(number + " is an Even Number");
+	        } else {
+	            System.out.println(number + " is an Odd Number");
+	        }
+
+	        sc.close();
+	    }
+
+}
